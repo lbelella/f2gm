881b8297eb61880169b6b6621ac7f905eb38c895
LB Belella <lb.belella@live.com>
2021-11-12 22:57:16 -0500

    - Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 
 PKG_NAME=f2gm.tgz
 
+cd "$(dirname "$0")"
+
+cd python
+./map.py
+cd ../
+npm run build
+
 rm -rf $PKG_NAME
 tar -zcvf $PKG_NAME index.html dist
 scp -i HTTPServer.pem $PKG_NAME ubuntu@f2gm.com:~
